fc3e3df7ad6edc5204152c2a752ce3187ec44767
Jimmy Xu <xjimmyshcn@gmail.com>
2016-09-03 22:38:28 +0800

    [master] add hyperhq/jenkins-slave-golang:1.7

---
+++ example/jenkins-slave-golang/1.7/README.md
@@ -0,0 +1,11 @@
+hyperhq/jenkins-slave-golang
+============================
+
+This is a jenkins slave image for build golang project.
+It contains:
+ - centos:7.2.1511
+ - java
+ - golang
+ - jenkins slave(REF: [jenkinsci/slave](https://hub.docker.com/r/jenkinsci/slave/))
+
+Usage: `./build.sh`
---
+++ example/jenkins-slave-golang/1.7/build.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e
+
+repo="hyperhq/jenkins-slave-golang"
+tag=1.7
+image=${repo}:${tag}
+
+
+
+function build(){
+    echo "starting build..."
+    echo "=============================================================="
+    docker build -t ${image} .
+}
+
+function push(){
+
+    echo -e "\nstarting push [${image}] ..."
+    echo "=============================================================="
+    docker push ${image}
+
+    echo -e "\nstarting push [${repo}:latest] ..."
+    echo "=============================================================="
+    docker tag ${image} ${repo}:latest
+    docker push ${repo}:latest
+}
+
+
+case "$1" in
+    "push")
+        build
+        push
+        ;;
+    "")
+        build
+        ;;
+    *)
+        cat <<EOF
+usage:
+    ./build.sh             # build only
+    ./build.sh push        # build and push
+EOF
+    exit 1
+        ;;
+esac
+
+
+
+echo -e "\n=============================================================="
+echo "Done!"
